679a81462199bf972339df6306e02f09591fcede
Lenovo <Lenovo@DESKTOP-I8SGQSI>
2025-03-30 02:09:18 +0300

    Add project files.

---
+++ WPF-LoginForm/Models/UserAccountModel.cs
@@ -0,0 +1,9 @@
+﻿namespace WPF_LoginForm.Models
+{
+    public class UserAccountModel
+    {
+        public string Username { get; set; }
+        public string DisplayName { get; set; }
+        public byte[] ProfilePicture { get; set; }
+    }
+}
